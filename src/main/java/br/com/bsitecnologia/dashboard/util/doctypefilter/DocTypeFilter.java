e88815988bbfa805f5f84bcbb22a1380f4889c6c
Bruno Nascimento <brunocn80@gmail.com>
2012-09-21 01:34:23 -0300

    acho que agora vai, doctype na força bruta

---
+++ src/main/java/br/com/bsitecnologia/dashboard/util/doctypefilter/DocTypeFilter.java
@@ -0,0 +1,43 @@
+package br.com.bsitecnologia.dashboard.util.doctypefilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class DocTypeFilter implements Filter {
+
+	public FilterConfig filterConfig;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if(request.getContentType() == null){
+			PrintWriter out = response.getWriter();
+			CharResponseWrapper wrapper = new CharResponseWrapper((HttpServletResponse) response);
+			chain.doFilter(request, wrapper);
+			String modifiedHtml = "<!DOCTYPE HTML>"+wrapper.toString();
+			response.setContentLength(modifiedHtml.getBytes().length);
+			out.write(modifiedHtml);
+			out.close();
+		}else{
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
